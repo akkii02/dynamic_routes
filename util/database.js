11cbe2316d2ca8fb6523f9419576736572aa2d5d
akkii02 <akshayakki01997@gmail.com>
2024-08-06 09:36:36 -0700

    task 21

--- util/database.js
+++ util/database.js
@@ -1,4 +1,8 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('node_complete','root','168179',{dialect:'mysql',host:'localhost'});
+
+const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete','168179' ,{
+  dialect: 'mysql',
+  host: 'localhost',
+});
 
 module.exports = sequelize;
